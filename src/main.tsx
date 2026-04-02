d874d4d2d8480ead997e2ddb7372a1afdd6bc37b
Amazigh-DZ <infovpp@nachhilfe-plus.de>
2026-04-02 20:06:31 +0200

    fix: bundle remaining local image assets

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import faviconImage from '../images/nachhilfe-plus-logo-quadratisch-removebg-2.png';
+
+const faviconLink = document.querySelector("link[rel='icon']") as HTMLLinkElement | null;
+
+if (faviconLink) {
+  faviconLink.href = faviconImage;
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
